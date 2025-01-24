6a3bd731df11a116e623fa8cf880b7879b085ee7
the3zero3 <thedini23@gmail.com>
2025-01-24 13:59:10 -0800

    Updated HTML, CSS and JS files with assignment requirements

--- VS-HTML-Assignment/wwwroot/js/site.js
+++ VS-HTML-Assignment/wwwroot/js/site.js
@@ -2,3 +2,10 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function grow() {
+    document.getElementById("animation").style.fontSize = "100px";
+}
+
+function shrink() {
+    document.getElementById("animation").style.fontSize = "calc(1.475rem + 2.7vw)";
+}
